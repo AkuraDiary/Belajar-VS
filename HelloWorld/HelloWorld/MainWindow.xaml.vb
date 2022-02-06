9fb899722553f9ac9c03b691ad0da13f31499fee
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-06 09:49:29 +0700

    yeye, hello world

--- HelloWorld/HelloWorld/MainWindow.xaml.vb
+++ HelloWorld/HelloWorld/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub btnClickThis_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
+        lblHelloWorld.Content = "Hello World!"
+    End Sub
 End Class
