fb0089aa50e92e3b923371c2a0558db690f8c502
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-11 18:23:41 +0700

    added message and close button handler

--- VBTutorial1/VBTutorial1/MainWindow.xaml.vb
+++ VBTutorial1/VBTutorial1/MainWindow.xaml.vb
@@ -2,4 +2,12 @@
     Private Sub btnOk_Click(sender As Object, e As RoutedEventArgs) Handles btnOk.Click
         txtName.Text = "Hello " & txtName.Text
     End Sub
+
+    Private Sub txtMessage_Click(sender As Object, e As RoutedEventArgs) Handles txtMessage.Click
+        MessageBox.Show("Hello " & txtName.Text, "Hello " & txtName.Text)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As RoutedEventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
