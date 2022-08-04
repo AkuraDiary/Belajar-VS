e7e34fde8b2fb8ba0ea22f152ed5528147eb137d
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-04 23:02:56 +0700

    alhamdulillah bisa ges

--- CodeFirstNewDatabaseSample/BloggingContext.cs
+++ CodeFirstNewDatabaseSample/BloggingContext.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 
+
 namespace CodeFirstNewDatabaseSample
 {
     public class BloggingContext : DbContext
--- CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace CodeFirstNewDatabaseSample
+﻿
+namespace CodeFirstNewDatabaseSample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                //reading and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                //Display all Blogs from the database
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+                Console.WriteLine("All blogs in the database: ");
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
         }
     }
 }
